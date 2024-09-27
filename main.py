407646464bf26704f4e8d345d079f46a2f308fcf
Holopura Anna <holopurana@knu.ua>
2024-09-27 09:41:20 +0300

    changes

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-from utils import factorial
+from utils import factorial, divided_by_5
 
 print(factorial(5))
+print(divided_by_5(25))
--- utils.py
+++ utils.py
@@ -3,3 +3,8 @@ def factorial(n):
         return 1
     else:
         return n * factorial(n-1)
+
+def divided_by_5(n):
+    if n%5 == 0:
+        return "Is divided by 5"
+    return "Is not divided by 5"
